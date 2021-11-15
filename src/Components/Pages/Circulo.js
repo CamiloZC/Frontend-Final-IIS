097271274f9f4fa62a3ab2230a126b361db6697f
CamiloZC <61916021+CamiloZC@users.noreply.github.com>
2021-11-14 23:59:22 -0500

    Final

--- src/Components/Pages/Circulo.js
+++ src/Components/Pages/Circulo.js
@@ -1,30 +1,74 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import '../../App.css'
 
-function Circulo() {
+const Circulo = (props) => {
+  const [calculatingPerimeter, setIsCalculatingPerimeter] = useState(false)
+  const [calculatingArea, setIsCalculatingArea] = useState(false)
+  const [canCalculate, setCanCalculate] = useState(false)
+  const [perimeter, setPerimeter] = useState(null)
+  const [area, setArea] = useState(null)
+  const [radio, setRadio] = useState(null)
+
+  const calculatePerimeter = async () => {
+    setIsCalculatingPerimeter(true)
+    axios.get(`http://localhost:3001/perimeter/circle?radio=${radio}`)
+    .then(({data}) => setPerimeter(data.perimeter))
+    .finally(() => setIsCalculatingPerimeter(false))
+  }
+
+  const calculateArea = async () => {
+    setIsCalculatingArea(true)
+    axios.get(`http://localhost:3001/area/circle?radio=${radio}`)
+    .then(({data}) => setArea(data.area))
+    .finally(() => setIsCalculatingArea(false))
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    return Promise.all([
+      calculatePerimeter(),
+      calculateArea()
+    ])
+  }
+
+  useEffect(() => {
+    setCanCalculate(radio && radio > 0)
+
+  }, [radio])
+
+  const handleChange = (event) => {
+    const radioValue = parseInt(event.target.value)
+    setRadio(radioValue)
+  }
+
+  const isCalculating = calculatingPerimeter || calculatingArea
+
   return (
     <div className="app-container">
       <div >
         <h1>Área y Perimetro del Círculo</h1>
       </div>
       <br/>
-      <form>
+      <form onSubmit={handleSubmit}>
             <div className="row">
                 <div className="form-group col-md-8">
                     <input type="text" className="form-control form-control lg"
-                    placeholder="Inserte el radio" />
+                    placeholder="Inserte el radio" min={0} onChange={handleChange} disabled={isCalculating}/>
                 </div>
                 <div className="form-group col-md-4">
-                    <input type="submit" class="btn btn-danger"
-                    value="Calcular" />
+                <button disabled={isCalculating || !canCalculate} type="submit" className="btn btn-danger">
+                        {isCalculating ? 'Calculando' : 'Calcular'}
+                        </button>
                 </div>
             </div>
         </form>
       <br/>
       <div className="app-results">
-        <h3>Área:</h3>
-        <h3>Perimetro:</h3>
-      </div>
+              <h3>Área: {calculatingArea ? 'Calculando el área' : area}</h3>
+              <h3>Perímetro: {calculatingPerimeter ? 'Calculando el perímetro' : perimeter}</h3>
+          </div>
     </div>
   );
 }
--- src/Components/Pages/Rectangulo.js
+++ src/Components/Pages/Rectangulo.js
@@ -1,43 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import '../../App.css'
 
-function Rectangulo() {
-  return (
-    <div className="app-container">
-      <div >
-        <h1>Área y Perimetro del Rectángulo</h1>
+const Rectangulo = (props) => {
+  const [calculatingPerimeter, setIsCalculatingPerimeter] = useState(false)
+  const [calculatingArea, setIsCalculatingArea] = useState(false)
+  const [canCalculate, setCanCalculate] = useState(false)
+  const [perimeter, setPerimeter] = useState(null)
+  const [area, setArea] = useState(null)
+  const [sides, setSides] = useState({
+    sideA : 0,
+    sideB : 0
+  })
+
+  const calculatePerimeter = async () => {
+    setIsCalculatingPerimeter(true)
+    axios.get(`http://localhost:3001/perimeter/rectangle?sideA=${sides.sideA}&sideB=${sides.sideB}`)
+    .then(({data}) => setPerimeter(data.perimeter))
+    .finally(() => setIsCalculatingPerimeter(false))
+  }
+
+  const calculateArea = async () => {
+    setIsCalculatingArea(true)
+    axios.get(`http://localhost:3001/area/rectangle?sideA=${sides.sideA}&sideB=${sides.sideB}`)
+    .then(({data}) => setArea(data.area))
+    .finally(() => setIsCalculatingArea(false))
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    return Promise.all([
+      calculatePerimeter(),
+      calculateArea()
+    ])
+  }
+
+  useEffect(() => {
+    const checkCanCalculate = Object.keys(sides).every(key => sides[key] && sides[key] > 0)
+
+    setCanCalculate(checkCanCalculate)
+
+  }, [sides])
+
+  const handleChange = (event, side) => {
+    const sideValue = parseInt(event.target.value)
+    setSides(currentSidesValues => ({
+      ...currentSidesValues,
+      [side]:sideValue
+    }))
+  }
+
+  const isCalculating = calculatingPerimeter || calculatingArea
+
+    return (
+      <div className="app-container">
+        <div >
+          <h1>Área y Perimetro del Rectángulo</h1>
+        </div>
+        <br/>
+        <div >
+          <h2>Inserta las medidas del Rectángulo:</h2>
+        </div>
+        <br/>
+        <form onSubmit={handleSubmit}>
+              <div className="app-input">
+                  <div className="form-group col-md-1"/>
+                  <div className="form-group col-md-2">
+                      <input type="number" className="form-control form-control lg"
+                      placeholder="Lado #1" min={0} onChange={e => handleChange(e, 'sideA')} disabled={isCalculating} />
+                  </div>
+                  <div className="form-group col-md-1"/>
+                  <div className="form-group col-md-2">
+                      <input type="number" className="form-control form-control lg"
+                      placeholder="Lado #2" min={0} onChange={e => handleChange(e, 'sideB')} disabled={isCalculating}/>
+                  </div>
+                  <div className="form-group col-md-1"/>
+                  <div className="form-group col-md-2">
+                      <button disabled={isCalculating || !canCalculate} type="submit" className="btn btn-danger">
+                        {isCalculating ? 'Calculando' : 'Calcular'}
+                        </button>
+                  </div>
+              </div>
+          </form>
+          <br/>
+          <div className="app-results">
+              <h3>Área: {calculatingArea ? 'Calculando el área' : area}</h3>
+              <h3>Perímetro: {calculatingPerimeter ? 'Calculando el perímetro' : perimeter}</h3>
+          </div>
       </div>
-      <br/>
-      <div >
-        <h2>Inserta las medidas del rectángulo:</h2>
-      </div>
-      <br/>
-      <form>
-            <div className="app-input">
-               
-                <div className="form-group col-md-4">
-                    <input type="text" className="form-control form-control lg"
-                    placeholder="Lado #1" />
-                </div>
-                <div className="form-group col-md-1"/>
-                <div className="form-group col-md-4">
-                    <input type="text" className="form-control form-control lg"
-                    placeholder="Lado #2" />
-                </div>
-                <div className="form-group col-md-1"/>
-                <div className="form-group col-md-2">
-                    <input type="submit" class="btn btn-danger"
-                    value="Calcular" />
-                </div>
-            </div>
-        </form>
-      <br/>
-      <div className="app-results">
-        <h3>Área:</h3>
-        <h3>Perimetro:</h3>
-      </div>
-    </div>  
-  );
+    );
 }
 
-export default Rectangulo;
+export default Rectangulo
